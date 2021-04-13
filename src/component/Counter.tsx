0624707961c47623f566b0dda8eb2fed1eeab7f5
노종화 <nicenoz@gmail.com>
2021-04-14 06:36:11 +0900

    test

--- src/component/Counter.tsx
+++ src/component/Counter.tsx
@@ -1,6 +1,6 @@
 import React, { MouseEventHandler } from 'react';
 
-function Counter(props:{ number:number, onIncrease:MouseEventHandler<HTMLButtonElement>, onDecrease:MouseEventHandler<HTMLButtonElement>}):JSX.Element {
+function Counter( props:{ number:number, onIncrease:MouseEventHandler<HTMLButtonElement>, onDecrease:MouseEventHandler<HTMLButtonElement>}):JSX.Element {
     return (
         <div>
             <h1>{props.number}</h1>
